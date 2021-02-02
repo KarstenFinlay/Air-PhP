424ad73c9e653151700c639562e38af267ba69a9
HolsDuckett <hf.duckett@gmail.com>
2021-02-02 15:37:09 +0000

    added unit test for viewing all listings

--- lib/listing.rb
+++ lib/listing.rb
@@ -2,6 +2,16 @@ require 'pg'
 
 class Listing
 
+  def self.all
+    if ENV['ENVIRONMENT'] = 'test'
+      connection = PG.connect(dbname: "airphp_test")
+    else
+      connection = PG.connect(dbname: "airphp")
+    end
+    result = connection.exec("SELECT * FROM listings;")
+    result.map { |listing| listing['name']}
+  end
+
   def self.create(name)
     if ENV['ENVIRONMENT'] = 'test'
       connection = PG.connect(dbname: "airphp_test")
@@ -10,9 +20,6 @@ class Listing
     end
 
     connection.exec("INSERT INTO listings (name) VALUES ('#{name}');")
-
-    # This is here just so that there are no incomplete tests in main
-    return "Bill Gates Island"
   end
 
 end
