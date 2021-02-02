54e3ff1ba82478bb30dc2b3751b31b40098eaab6
HolsDuckett <hf.duckett@gmail.com>
2021-02-02 17:34:38 +0000

    Updated controller to merge model and view features

--- lib/listing.rb
+++ lib/listing.rb
@@ -3,17 +3,18 @@ require 'pg'
 class Listing
 
   def self.all
-    if ENV['ENVIRONMENT'] = 'test'
+    if ENV['ENVIRONMENT'] == 'test'
       connection = PG.connect(dbname: "airphp_test")
     else
       connection = PG.connect(dbname: "airphp")
     end
     result = connection.exec("SELECT * FROM listings;")
     result.map { |listing| listing['name']}
+
   end
 
-  def self.create(name)
-    if ENV['ENVIRONMENT'] = 'test'
+  def self.create(name:)
+    if ENV['ENVIRONMENT'] == 'test'
       connection = PG.connect(dbname: "airphp_test")
     else
       connection = PG.connect(dbname: "airphp")
