f7b44773d1e7c28a54959f7d008dc27473896fdd
HolsDuckett <hf.duckett@gmail.com>
2021-02-03 11:23:36 +0000

    added feature test for requesting to book a listing

--- spec/features/create_listing_spec.rb
+++ spec/features/create_listing_spec.rb
@@ -1,8 +1,8 @@
 feature 'Creates property listing' do
-    scenario 'Adds a listing to the page' do
-        visit ('/listings/new')
-        fill_in "name",	with: "Test property"
-        click_button "Submit"
-        expect(page).to have_content("Test property")
-    end
+  scenario 'Adds a listing to the page' do
+      visit ('/listings/new')
+      fill_in "name",	with: "Test property"
+      click_button "Submit"
+      expect(page).to have_content("Test property")
+  end
 end
